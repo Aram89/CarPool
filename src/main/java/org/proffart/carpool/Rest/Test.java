a9aff762a5354b3138c0f5e992305ff8a5ecad7f
Aram89 <aramkirakosyan89@gmail.com>
2015-03-27 01:41:08 -0700

    test

---
+++ src/main/java/org/proffart/carpool/Rest/Test.java
@@ -0,0 +1,7 @@
+package org.proffart.carpool.Rest;
+
+/**
+ * Created by Aram on 3/27/2015.
+ */
+public class Test {
+}
---
+++ src/main/java/org/proffart/carpool/domain/TestEntitY.java
@@ -0,0 +1,7 @@
+package org.proffart.carpool.domain;
+
+/**
+ * Created by Aram on 3/27/2015.
+ */
+public class TestEntitY {
+}
