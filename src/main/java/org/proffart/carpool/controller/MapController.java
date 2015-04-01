9c5045b8bea9c74850ed353766bdc45964b9a884
Aram89 <aramkirakosyan89@gmail.com>
2015-04-01 02:05:09 -0700

    Added  test for controller

--- src/main/java/org/proffart/carpool/controller/MapController.java
+++ src/main/java/org/proffart/carpool/controller/MapController.java
@@ -1,7 +1,21 @@
 package org.proffart.carpool.controller;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Aram on 3/27/2015.
  */
+
+@Controller
 public class MapController {
+
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    public String getEvents(ModelMap model){
+        return "test";
+    }
+
 }
