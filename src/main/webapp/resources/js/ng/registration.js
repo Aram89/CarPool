0fa28d0678395a3ce5d1ae122381f5630b6e6a3a
ashmna <mnatsakanyan.ashot@gmail.com>
2015-04-18 00:39:41 +0400

    - added Registration Page  - added bootstrap

---
+++ src/main/webapp/resources/js/ng/registration.js
@@ -0,0 +1,6 @@
+alert(1);
+var registration = angular.module('registration', []);
+registration.controller('registrationController', function($scope) {
+	alert(2);
+	//$scope.userType = 'guest';
+});
