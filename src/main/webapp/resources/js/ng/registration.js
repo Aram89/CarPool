cb1f8593ad6839c73e4b94a0791f4460a92f5ce1
ashmna <mnatsakanyan.ashot@gmail.com>
2015-04-24 23:11:52 +0400

    angular remot check

--- src/main/webapp/resources/js/ng/registration.js
+++ src/main/webapp/resources/js/ng/registration.js
@@ -1,12 +1,94 @@
-var registration = angular.module('registration', []);
-registration.controller('registrationController', function($scope, $http) {
+(function () {
+	'use strict';
+
+	var registration = angular.module('registration', []);
 	
-	$scope.registration = function() {
-		$http.post('' , $scope.user)
-		.success(function(data) {
-			console.log(data);
-		});
-	};
+	registration.controller('registrationController', function($scope, $http) {
+		
+		$scope.registration = function() {
+			$http.post('http://localhost/test.php' , $scope.user)
+			.success(function(data) {
+				console.log(data);
+			});
+		};
+		
+		
+	});
 	
+	registration.directive('ngRemoteCheck', ['$http', function ($http) {
+		return {
+			restrict : 'A',
+			require  : '?ngModel',
+			link     : function (scope, elem, attrs, ctrl) {
+				if (!ctrl) return;
+				if (!attrs['ngRemoteCheck']) return;
+				
+				var checkUrl = attrs['ngRemoteCheck'],
+				    checkDuration = 333, oldVal = '', propName = 'name',
+				    timeoutId , isValid = false;
+				if(attrs['id']) propName = attrs['id'];
+				if(attrs['name']) propName = attrs['name'];
+
+				
+
+				var validator = function (value) {
+					isValid = oldVal == value;
+					if(!value) isValid = true;
+					if(!isValid) {
+						if(timeoutId) clearTimeout(timeoutId);
+						timeoutId = setTimeout(function(){
+							$http.get(checkUrl + '?'+propName+'='+value)
+							.success(function(data, status, headers, config) {
+								console.log(data);
+								console.log(status);
+								console.log(headers);
+								console.log(config);
+								isValid = data;
+								ctrl.$setValidity('remote', isValid);
+								if(isValid) oldVal = value;
+							});
+						}, checkDuration);
+					}
+					ctrl.$setValidity('remote', isValid);
+					return value;
+				}
+
+				ctrl.$parsers.unshift(validator);
+				ctrl.$formatters.push(validator);
+				attrs.$observe('ngRemoteCheck', function () {
+					validator(ctrl.$viewValue);
+				});
+
+			}
+		};
+	}]);
 	
-});
+	registration.directive('ngMatch', ['$parse', function ($parse) {
+
+		return {
+			restrict : 'A',
+			require  : '?ngModel',
+			link     : function (scope, elem, attrs, ctrl) {
+				if (!ctrl) return;
+				if (!attrs['ngMatch']) return;
+
+				var firstPassword = $parse(attrs['ngMatch']);
+
+				var validator = function (value) {
+					var temp = firstPassword(scope),
+					v = value === temp;
+					ctrl.$setValidity('match', v);
+					return value;
+				}
+
+				ctrl.$parsers.unshift(validator);
+				ctrl.$formatters.push(validator);
+				attrs.$observe('ngMatch', function () {
+					validator(ctrl.$viewValue);
+				});
+
+			}
+		};
+
+	}]);
+})();
