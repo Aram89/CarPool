5f494130549ecb5abe5a6330ef8a2c6e973c0de4
Aram89 <aramkirakosyan89@gmail.com>
2015-08-15 15:20:50 -0700

    Added profiles for build.

--- src/main/java/org/proffart/carpool/dao/UserDAOImpl.java
+++ src/main/java/org/proffart/carpool/dao/UserDAOImpl.java
@@ -1,16 +1,23 @@
 package org.proffart.carpool.dao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.proffart.carpool.domain.User;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
 import java.sql.SQLException;
 
 public class UserDAOImpl extends SqlMapClientDaoSupport implements UserDAO {
+
+    private static Logger logger = LogManager.getLogger(UserDAOImpl.class);
+
+
     public void insert(User user) throws SQLException {
         Integer id = (Integer) getSqlMapClient().insert("user.insert", user);
     }
 
     public Integer getCount(String userName) throws SQLException {
+        logger.info("Check email for user:" + userName);
         return (Integer)getSqlMapClient().queryForObject("user.checkUserName", userName);
     }
 
